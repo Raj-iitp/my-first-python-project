e45b60efb81ed951833e01c1009167ee59ce5de8
Raj-iitp <rajkumardos5634@gmail.com>
2026-07-16 21:17:02 +0530

    Add files via upload

---
+++ traffic_system.py
@@ -0,0 +1,9 @@
+str1 = "raj kumar"
+str2 = "choudhary"
+print(str1+str2)
+str = "raj kumar choudhary"
+print(str.capitalize())
+print(str.endswith("dhary"))
+print(str.find("a"))
+print(str.count("a"))
+print(str.replace("choudhary", "sahani"))
